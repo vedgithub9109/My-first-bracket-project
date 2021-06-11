8bde47135160009a67cb1d8475cb2c01cc259143
vedgithub9109 <84722506+vedgithub9109@users.noreply.github.com>
2021-06-11 13:57:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function change() {
+    document.getElementById("ved").src= "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/bf1cfe078867bd9e1a3bd4c7fe77150245b1ba08b13b5937cc89b5f22163b31f._V_SX300_.jpg";
+}
